a96a3a894b63aad3802e2ad4202e5ab48acb41fd
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-07-15 04:11:33 +0300

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import run, cd
 def update():
     with cd('/root/deploy/'):
         run('git pull')
+        run('docker-compose pull')
         run('docker-compose stop')
-        run('docker-compose rm')
-        run('docker-compose up -d')
+        run('docker-compose rm -f')
+        run('docker-compose up -d --force-recreate')
